f807372c8af4a1f950e6f62ec8f3e48b860dad40
Angga Ari Wijaya <anggadarkprince@gmail.com>
2025-12-02 09:08:10 +0700

    feat(template): implement layout template

--- utilities/template.go
+++ utilities/template.go
@@ -1,15 +1,73 @@
 package utilities
 
 import (
+	"database/sql"
 	"fmt"
+	"html"
 	"net/http"
 	"os"
 	"path/filepath"
+	"reflect"
+	"slices"
 	"strings"
 	"text/template"
 )
 
 var Template *template.Template
+
+var TemplateFuncs = template.FuncMap{
+    "add": func(a, b int) int { return a + b },
+    "toUpper": strings.ToUpper,
+    "hasPrefix": strings.HasPrefix,
+    "contains": func(arr []string, value string) bool {
+        return slices.Contains(arr, value)
+    },
+    "containsByField": func(list any, fieldName string, value any) bool {
+        v := reflect.ValueOf(list)
+
+        // Check list is a slice
+        if v.Kind() != reflect.Slice {
+            return false
+        }
+
+        for i := 0; i < v.Len(); i++ {
+            elem := v.Index(i)
+
+            // Check elem adalah struct
+            if elem.Kind() == reflect.Ptr {
+                elem = elem.Elem()
+            }
+            if elem.Kind() != reflect.Struct {
+                continue
+            }
+
+            // Get field by name
+            field := elem.FieldByName(fieldName)
+            if !field.IsValid() {
+                continue
+            }
+
+            // Check field with value then convert to string
+            if fmt.Sprint(field.Interface()) == fmt.Sprint(value) {
+                return true
+            }
+        }
+
+        return false
+    },
+    "default": func(value, fallback string) string {
+        if value == "" {
+            return fallback
+        }
+        return value
+    },
+    "formatDate": func(v any, layout, fallback string) string {
+        if t, ok := v.(sql.NullTime); ok && t.Valid {
+            return t.Time.Format(layout)
+        }
+        return fallback
+    },
+}
 
 func LoadTemplates() *template.Template {
     root := template.New("").Option("missingkey=error").Funcs(TemplateFuncs)
@@ -78,5 +136,18 @@ func Render(w http.ResponseWriter, r *http.Request, name string, data any) error
         payload["data"] = data
     }
 
-	return Template.ExecuteTemplate(w, name, payload)
+    // 
+    tmpl := template.Must(Template.Clone())
+    bytes, _ := os.ReadFile("views/" + name)
+    tmpl = template.Must(tmpl.Parse(string(bytes)))
+
+	return tmpl.ExecuteTemplate(w, name, payload)
+}
+
+func EscapeHTML(s string) string {
+    return html.EscapeString(s)
+}
+
+func UnescapeHTML(s string) string {
+    return html.UnescapeString(s)
 }
